84fff57a98c1b4acd97ef893894d93cb3a6adb96
ice <1298364867@qq.com>
2018-01-15 15:14:11 +0800

    第三版本(事件监听)
    根据用户的按键转向相应的事件响应方法

---
+++ ThirdWork/NoteBookConsole1/Function.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoteBookConsole1
+{
+    class Function
+    {
+        public delegate void NoteEventHandler(Object sender, EventArgs e);//声明委托
+        public event NoteEventHandler NoteEvent;//声明事件
+        public void Functioning()
+        {
+            //建立NoteEventArgs对象
+            EventArgs e = new EventArgs();
+            if (NoteEvent != null)
+            {
+                //如果有对象注册
+                NoteEvent(this, e);//调用所有注册对象的方法 }
+            }
+        }
+
+    }
+}
---
+++ ThirdWork/NoteBookConsole1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoteBookConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //调用Menu类中的show()输出主菜单界面
+            Menu mymenu = new Menu();
+            Function myfunction = new Function();
+            OptionFunction optionfunction = new OptionFunction();
+
+            myfunction.NoteEvent += mymenu.Display;
+            myfunction.Functioning();
+            optionfunction.Option();
+            //调用Menu类中的Function()对于不同的菜单操作调用不同的方法
+            Console.ReadLine();
+        }
+    }
+
+}
